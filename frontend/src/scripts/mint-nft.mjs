3bb7ae2ae72edbcec3307abf239b5495ea85fc23
Heisenberg-737 <pranay.kothari17@gmail.com>
2023-02-12 19:35:22 +0530

    mint NFT fix

--- frontend/src/scripts/mint-nft.mjs
+++ frontend/src/scripts/mint-nft.mjs
@@ -5,7 +5,7 @@ async function mintNFT() {
   // const ExampleNFT = await ethers.getContractFactory("PetitionNFT");
   // const [owner] = await ethers.getSigners();
   // await ExampleNFT.attach("0xE0ac88897a6b3137e561B79B5573222c7F4EF35F").mintNFT(owner.address, "ipfs://bafyreifnck6yo4uv2zazokivlbtuhh7cdvw4qgyhb7jct4cjxq75eh33hy/metadata.json");
-  console.log("NFT minted to: ", owner.address);
+  // console.log("NFT minted to: ", owner.address);
 }
 
 // mintNFT(CONTRACT_ADDRESS, META_DATA_URL)
